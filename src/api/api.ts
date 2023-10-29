875fddc692a71efca646f48b73ad55224430104d
tulolo287 <iv1055@yandex.ru>
2023-10-30 00:16:44 +0300

    Updates

--- src/api/api.ts
+++
@@ -1,5 +0,0 @@
-export const getPosts = async () => {
-  await fetch("https://jsonplaceholder.typicode.com/posts")
-    .then((response) => response.json())
-    .then((data) => console.log(data));
-};
